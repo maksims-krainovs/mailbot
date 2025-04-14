5f8dab536bde288c7bcac56956b6e481f63d2b73
maksims.krainovs@toptal.com <maksims.krainovs@toptal.com>
2025-04-14 16:24:20 +0300

    initial project setup

---
+++ src/__tests__/util.spec.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from "vitest";
+import { util } from "../util.js";
+
+describe("sum function", () => {
+  it("should add two positive numbers correctly", () => {
+    expect(util()).toBe("Hello");
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+const s: string = "Hello !!";
+console.log(s);
